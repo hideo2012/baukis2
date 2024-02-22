bf1419867b7a60bfe9af3f9405b381c277610d56
hideo-sz5 <hideo.chou@gmail.com>
2024-02-22 10:07:44 +0000

    new token regenerate github.

--- config/routes.rb
+++ config/routes.rb
@@ -34,6 +34,7 @@ Rails.application.routes.draw do
           get :inbound, :outbound, :deleted, on: :collection
         end
       end
+      test for new token 20240222  nikkei  ath !!!
 =end
     end
   end
